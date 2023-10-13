8ed2633ff48fdd84a5f7a54a652b798b00cd899f
Denzelle Walker <dwalker@jetlifetech.com>
2023-10-13 12:57:03 -0500

    chore: added requirements.txt

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from generate_prompts import generate_prompts
 from openai_adapter import openai_chat
-
-load_dotenv()
 
 
 messages = generate_prompts("Google", "Software Engineer", "We are looking for a software engineer to join our team. You will be responsible for maintaining and improving our backend infrastructure.")
